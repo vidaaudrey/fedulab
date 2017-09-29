cebf04172c7c131c1ab9a550c7c6b5f3c52bced6
Audrey Li <ali@coursera.org>
2017-09-28 20:56:48 -0700

    chore: fix eslint error and add example

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,16 @@
 module.exports = {
   root: true,
   parser: 'babel-eslint',
-  plugins: ['react', 'jest', 'flowtype'],
-  extends: ['prettier', 'airbnb'],
+  plugins: ['react', 'jest', 'flowtype', 'jsx-a11y'],
+  extends: ['prettier', 'airbnb', 'react-app', 'plugin:jsx-a11y/recommended'],
   rules: {
     'react/jsx-uses-react': 2,
     'react/jsx-uses-vars': 2,
     'react/jsx-filename-extension': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/no-unresolved': 0,
+    'import/extensions': 0,
   },
   globals: {
     toJson: true,
